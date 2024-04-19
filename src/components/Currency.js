95864eb0f14259e9877f8df4098b996c8ec003cd
feramig <feram_caa@hotmail.com>
2024-04-19 01:52:09 -0400

    Change Styles

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -11,17 +11,34 @@ const Currency = () => {
             })
     }
     
-
+    const colorStyles = {
+        color:"black",
+        backgroundColor:"#8cf292",
+        ':focus': {
+            color: '#ffffff'
+          }
+    }
+    const selectStyles = {
+        color:"white", 
+        backgroundColor:"#8cf292", 
+        borderColor:"white"
+    }
   return (
         <div className='alert alert-secondary'>
-      <select className='alert-success' name="Currency" id="Currency" onChange={event=>changeCurrency(event.target.value)}>
-        <option defaultValue>Currency</option>
-        <option value="$">$ Dollar</option>
-        <option value="£">£ Pound</option>
-        <option value="€">€ Euro</option>
-        <option value="₹">₹ Ruppee</option>
-      </select>	
-    </div>
+            <label>
+                Currency
+                <select className='alert-success' 
+                    name="Currency" 
+                    id="Currency" 
+                    onChange={event=>changeCurrency(event.target.value)}
+                    style={selectStyles}>
+                <option value="$" style={colorStyles}>$ Dollar</option>
+                <option value="£" style={colorStyles}>£ Pound</option>
+                <option value="€" style={colorStyles}>€ Euro</option>
+                <option value="₹" style={colorStyles}>₹ Ruppee</option>
+            </select>
+            </label> 	
+        </div>
     );
 };
 
